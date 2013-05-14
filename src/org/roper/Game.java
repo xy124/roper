1e3244cb1bead2511494bb667d50b88793a0796f
xy124 <qcd@gmx.net>
2013-05-14 19:04:34 +0200

    jeah its a window now!

--- src/org/roper/Game.java
+++ src/org/roper/Game.java
@@ -1,11 +1,25 @@
 package org.roper;
 
-public class Game implements IGameObject {
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+public class Game extends Frame implements IGameObject, Runnable  {
+
+	
+	
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-
+		
+		setSize(800,800);
+		setVisible(true);
+		addWindowListener(new WindowAdapter() {
+			  public void windowClosing(WindowEvent we) {
+			    System.exit(0);
+			  }
+			});
 	}
 
 	@Override
@@ -25,7 +39,39 @@ public class Game implements IGameObject {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Game game = new Game();
+		game.init();
 	}
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**Doppelpufferung**/
+	// Definition zweier Instanzvariablen für die Doppelpufferung im Kopf des Programmes
+	private Image dbImage;
+	private Graphics dbg;
+	/** Update - Methode, Realisierung der Doppelpufferung zur Reduzierung des Bildschirmflackerns */
+	public void update (Graphics g)	{	      
+	      // Bildschirm im Hintergrund löschen
+	      dbg.setColor (getBackground ());
+	      dbg.fillRect (0, 0, this.getSize().width, this.getSize().height);
+
+	      // Auf gelöschten Hintergrund Vordergrund zeichnen
+	      dbg.setColor (getForeground());
+	      paint (dbg);
+
+	      // Nun fertig gezeichnetes Bild Offscreen auf dem richtigen Bildschirm anzeigen
+	      g.drawImage (dbImage, 0, 0, this);
+	}
+
+	public void paint (Graphics g) {		
+		
+		g.drawString("Punkte: ", 100, 100);
+		g.drawString("Zeit: "+" von 2250", 0, 20);
+		//g.drawString("HZ: "+flyingbirds, 0, 50);
+	}
+	
 }
